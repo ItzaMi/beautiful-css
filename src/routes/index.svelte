e926532f0902e7945647d84faf547784ce287a35
ItzaMi <rros00@gmail.com>
2021-09-18 14:35:44 +0100

    [index] add some sort of media query to title

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -7,22 +7,11 @@
 	<title>Home</title>
 </svelte:head>
 
-<!-- <div class="h-screen flex flex-row justify-left items-center">
-	<div class="w-2/4">
-		<h3 class="text-3xl">Welcome to</h3>
-		<h1 class="text-6xl font-black">Beautiful CSS</h1>
-		<p class="mt-8 w-3/4">
-			Here's a curated list of different components and CSS styles for you to use on your next work
-		</p>
-	</div>
-	<div class="w-2/4 h-full  flex flex-col justify-center items-center">
-		<div class="w-52 h-52 rounded-md" style="box-shadow: rgba(0, 0, 0, 0.09) 0px 3px 12px;" />
-	</div>
-</div> -->
-
 <div class="relative h-screen flex flex-col justify-center items-center">
 	<h3 class="text-3xl">Welcome to</h3>
-	<h1 class="text-9xl font-black">Beautiful <span class="transform rotate-45">CSS</span></h1>
+	<h1 class="text-5xl md:text-8xl sm-md:text-9xl font-black">
+		Beautiful <span class="transform rotate-45">CSS</span>
+	</h1>
 	<div class="mt-8 w-2/5 text-center">
 		<p>
 			A curated list of CSS styles and components that you can easily copy and use on your projects

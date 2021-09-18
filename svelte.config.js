607c0bd0813fb773e3a684f2909068230f354b14
ItzaMi <rros00@gmail.com>
2021-09-18 12:29:36 +0100

    [svelte] add `adapter` to properly build

--- svelte.config.js
+++ svelte.config.js
@@ -1,8 +1,13 @@
+import node from '@sveltejs/adapter-node';
+
+// https://kit.svelte.dev/docs#adapters
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
 		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte'
+		target: '#svelte',
+		adapter: node()
 	}
 };
 

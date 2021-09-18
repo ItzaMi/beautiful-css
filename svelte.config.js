a2938464d0a4965ea372dd2da03a47dd65a26df0
ItzaMi <rros00@gmail.com>
2021-09-18 12:37:29 +0100

    [svelte] change `adapter`

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import node from '@sveltejs/adapter-node';
+const netlify = require('@sveltejs/adapter-netlify');
 
 // https://kit.svelte.dev/docs#adapters
 
@@ -7,7 +7,7 @@ const config = {
 	kit: {
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte',
-		adapter: node()
+		adapter: netlify()
 	}
 };
 

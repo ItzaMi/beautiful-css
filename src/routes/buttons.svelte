1590695d103a6164ebc4da62e1d6141ebffe2108
ItzaMi <rros00@gmail.com>
2021-09-20 15:21:35 +0100

    [box-shadows / buttons] improve copy

--- src/routes/buttons.svelte
+++ src/routes/buttons.svelte
@@ -9,7 +9,8 @@
 	<div class="flex flex-col justify-center items-center">
 		<div class="w-1/2 text-center">
 			<h1 class="mt-32 mb-4 text-3xl font-bold">Hey there! 👋</h1>
-			<p class="m-0 ">Here are some buttons for you to use on your next work!</p>
+			<p class="m-0">Here are some buttons for you to use on your next work!</p>
+			<p class="m-0">Click on one of them to copy its style to your clipboard!</p>
 		</div>
 	</div>
 	<div class="mt-16 flex flex-row flex-wrap justify-around gap-20">
